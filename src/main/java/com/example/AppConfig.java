703287730fd5663c4a1a871109d5351d755b6434
yuanshihua <946896963@qq.com>
2017-11-24 12:30:37 +0800

    新增课堂作业

--- src/main/java/com/example/AppConfig.java
+++ src/main/java/com/example/AppConfig.java
@@ -4,11 +4,13 @@ import javax.sql.DataSource;
 
 import org.apache.commons.dbcp2.BasicDataSource;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
 
 @ImportResource("spring-context.xml")
 @Configuration
+@ComponentScan("com.example")//常用方法
 public class AppConfig {
 
 	@Bean
@@ -29,5 +31,20 @@ public class AppConfig {
 		ds.setUrl("jdbc:mariadb://localhost:3306/app");
 		return ds;
 	}
+	
+//	@Bean
+//	public SampleBean sampleBean() {
+//		SampleBean sampleBean = new SampleBean();
+//		sampleBean.setName("heheda");
+//		return sampleBean;
+//	}
+//	
+//	@Bean
+//	public TestBean testBean() {
+//		TestBean testBean = new TestBean();
+//		testBean.setAge(20);
+//		testBean.setName("zhangsan");
+//		return testBean;
+//	}
 
 }
